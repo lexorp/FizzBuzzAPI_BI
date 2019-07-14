c562e8e9c956dc65eae0e2c588107b0d77fa94f9
Alex Ortega <aop.alexortega@gmail.com>
2019-07-14 20:12:30 +0200

    test

--- FizzBuzzAPI/Controllers/ValuesController.cs
+++ FizzBuzzAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace FizzBuzzAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return "value " + id;
         }
 
         // POST api/values
